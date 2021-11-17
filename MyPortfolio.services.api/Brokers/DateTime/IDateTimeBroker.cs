bb44fb86748e8db7023f60abb23dc57942966073
Nighila <nighiladevi@gmail.com>
2021-11-17 15:17:32 -0500

    Broker: Date Time

---
+++ MyPortfolio.services.api/Brokers/DateTime/IDateTimeBroker.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace MyPortfolio.Services.Api.Brokers.DateTime
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
