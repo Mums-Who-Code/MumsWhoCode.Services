f2380c2e46cbf7c67085308eafdb5a0e036b4de3
Nighila <nighiladevi@gmail.com>
2021-11-21 01:37:55 -0500

    CODE RUB: Code Clenup

--- MyPortfolio.services.api/Controllers/HomeController.cs
+++ MyPortfolio.services.api/Controllers/HomeController.cs
@@ -9,11 +9,10 @@ namespace MyPortfolio.Services.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-
     public class HomeController : RESTFulController
     {
         [HttpGet]
         public ActionResult<string> Get() =>
-        Ok("Check whether System or API End points are responding properly or not");
+            Ok("Check whether System or API End points are responding properly or not");
     }
 }
