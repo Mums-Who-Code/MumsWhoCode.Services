290ae574a64cf0dfbbae1b03c3f2fe0ac8427095
Nighila <nighiladevi@gmail.com>
2021-11-21 00:26:23 -0500

    CONTROLLER: Home

---
+++ MyPortfolio.services.api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace MyPortfolio.Services.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+        Ok("Check whether System or API End points are responding properly or not");
+    }
+}
