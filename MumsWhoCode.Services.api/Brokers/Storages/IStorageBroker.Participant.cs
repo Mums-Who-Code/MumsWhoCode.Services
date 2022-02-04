b5798fe697af99dee8cd28cf54939db119bec5aa
Nighila <nighiladevi@gmail.com>
2022-02-03 19:37:41 -0700

    BROKER: Insert Participant

--- MumsWhoCode.Services.api/Brokers/Storages/IStorageBroker.Participant.cs
+++ MumsWhoCode.Services.api/Brokers/Storages/IStorageBroker.Participant.cs
@@ -2,9 +2,13 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using MumsWhoCode.Services.Api.Models.Participants;
+
 namespace MumsWhoCode.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Participant> InsertCategoryAsync(Participant participant);
     }
 }
